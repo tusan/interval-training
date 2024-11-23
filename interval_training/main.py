12580aedca4d803eaab324bc69bf57e473a85b7c
tusan <tubelli.santolo@gmail.com>
2024-11-23 18:56:54 +0100

    Minor refactorig to improve random interval questions

--- interval_training/main.py
+++ interval_training/main.py
@@ -1,36 +1,39 @@
+from __future__ import annotations
+
 import random
 from collections import deque
 
 import click
 
 CIRCLE_OF_FIFTH_MAJOR_SCALES = {
-    "C": ["C", "D", "E", "F", "G", "A", "B"],
-    "G": ["G", "A", "B", "C", "D", "E", "F#"],
-    "F": ["F", "G", "A", "Bb", "C", "D", "E"],
-    "D": ["D", "E", "F#", "G", "A", "B", "C#"],
-    "Bb": ["Bb", "C", "D", "Eb", "F", "G", "A"],
-    "A": ["A", "B", "C#", "D", "E", "F#", "G#"],
-    "Eb": ["Eb", "F", "G", "Ab", "Bb", "C", "D"],
-    "E": ["E", "F#", "G#", "A", "B", "C#", "D#"],
-    "Ab": ["Ab", "Bb", "C", "Db", "Eb", "F", "G"],
-    "B": ["B", "C#", "D#", "E", "F#", "G#", "A#"],
-    "Db": ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"],
-    "F#": ["F#", "G#", "A#", "B", "C#", "D#", "E#"],
-    "Gb": ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"],
-    "Cb": ["Cb", "Db", "Eb", "Fb", "Gb", "Ab", "Bb"],
-    "C#": ["C#", "D#", "E#", "F#", "G#", "A#", "B#"],
+    'C': ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
+    'G': ['G', 'A', 'B', 'C', 'D', 'E', 'F#'],
+    'F': ['F', 'G', 'A', 'Bb', 'C', 'D', 'E'],
+    'D': ['D', 'E', 'F#', 'G', 'A', 'B', 'C#'],
+    'Bb': ['Bb', 'C', 'D', 'Eb', 'F', 'G', 'A'],
+    'A': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'],
+    'Eb': ['Eb', 'F', 'G', 'Ab', 'Bb', 'C', 'D'],
+    'E': ['E', 'F#', 'G#', 'A', 'B', 'C#', 'D#'],
+    'Ab': ['Ab', 'Bb', 'C', 'Db', 'Eb', 'F', 'G'],
+    'B': ['B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#'],
+    'Db': ['Db', 'Eb', 'F', 'Gb', 'Ab', 'Bb', 'C'],
+    'F#': ['F#', 'G#', 'A#', 'B', 'C#', 'D#', 'E#'],
+    'Gb': ['Gb', 'Ab', 'Bb', 'Cb', 'Db', 'Eb', 'F'],
+    'Cb': ['Cb', 'Db', 'Eb', 'Fb', 'Gb', 'Ab', 'Bb'],
+    'C#': ['C#', 'D#', 'E#', 'F#', 'G#', 'A#', 'B#'],
 }
 
-INTERVALS = [*range(1, 10), 11, 13]
+INTERVALS = [*range(1, 9), 9, 11, 13]
 
-ALTERATIONS = ["", "b", "#", "bb", "x"]
-ALTERATIONS_WEIGHT = [100, 20, 20, 10, 10]
+ALTERATIONS = ['', 'b', '#', 'bb', 'x']
+ALTERATIONS_WEIGHT = [100, 20, 20, 2, 2]
 
 
 def get_interval(tune: str, interval: int, alteration: str) -> str:
     if tune not in CIRCLE_OF_FIFTH_MAJOR_SCALES:
         raise ValueError(
-            f"Provide only tunes from circle of fifth: {CIRCLE_OF_FIFTH_MAJOR_SCALES.keys()}"
+            f"Provide only tunes from circle of fifth:"
+            f" {CIRCLE_OF_FIFTH_MAJOR_SCALES.keys()}",
         )
     if interval not in INTERVALS:
         raise ValueError(f"Accepted intervals: {INTERVALS}")
@@ -39,10 +42,10 @@ def get_interval(tune: str, interval: int, alteration: str) -> str:
 
     return (
         (
-            f"{correct_interval}{alteration}".replace("##", "x")
-            .replace("#b", "")
-            .replace("b#", "")
-            .replace("#x", "x#")
+            f"{correct_interval}{alteration}".replace('##', 'x')
+            .replace('#b', '')
+            .replace('b#', '')
+            .replace('#x', 'x#')
         )
         if alteration
         else correct_interval
@@ -62,10 +65,12 @@ class QuestionTracker:
 
     def stats(self) -> str:
         result = f"\nRESULT:\n{self.__correct_count} correct answers\n\n"
-        return f"{result}Wrong answers:\n{'\n'.join([
-            f"question {question}th, answer {answer} but was {correct_answer}"
-            for question, answer, correct_answer in self.__wrong_answers
-        ])}" if self.__wrong_answers else result
+        return f"{result}Wrong answers:\n{
+            '\n'.join([
+                f"question {question} th, answer {answer} but was {correct_answer}"
+                for question, answer, correct_answer in self.__wrong_answers
+            ])
+        }" if self.__wrong_answers else result
 
 
 class QuestionBuilder:
@@ -74,16 +79,19 @@ class QuestionBuilder:
         self.__tune = tune
 
         self.__question_tracker = QuestionTracker()
-        self.__already_seen = deque(maxlen=7)
+        self.__already_seen: deque = deque(maxlen=7)
 
     def build_question(self) -> tuple[int, str]:
-        for i in range(0, 100):
-            interval, alteration = self.__build_question()
-            k = f"{interval}{alteration}"
-            if k not in self.__already_seen:
-                self.__already_seen.append(k)
+        interval, alteration = self.__build_question()
+
+        for _ in range(20, 0, -1):
+            if interval not in self.__already_seen:
+                self.__already_seen.append(interval)
                 return interval, alteration
-        raise ValueError("Seems you already checked all the possibility")
+
+            interval, alteration = self.__build_question()
+
+        return interval, alteration
 
     def __build_question(self) -> tuple[int, str]:
         return (
@@ -91,10 +99,9 @@ class QuestionBuilder:
             (
                 random.choices(ALTERATIONS, weights=ALTERATIONS_WEIGHT)[0]
                 if self.__include_alterations
-                else ""
+                else ''
             ),
         )
-
 
     def register_answer(self, interval: int, alteration: str, answer: str) -> None:
         correct_answer = get_interval(self.__tune, interval, alteration)
@@ -103,7 +110,7 @@ class QuestionBuilder:
             self.__question_tracker.right()
         else:
             self.__question_tracker.wrong(
-                f"{interval}{alteration}", answer, correct_answer
+                f"{interval}{alteration}", answer, correct_answer,
             )
 
     def finalize(self) -> str:
@@ -115,9 +122,9 @@ def interval_training(count: int, tune: str, include_alterations: bool) -> None:
 
     print(f"Tune: {tune}")
     try:
-        for i in range(1, count + 1):
+        for _ in range(1, count + 1):
             interval, alteration = question_builder.build_question()
-            answer = click.prompt(f"{interval}{alteration}th")
+            answer = click.prompt(f"{interval}{alteration} th")
             question_builder.register_answer(interval, alteration, answer)
     finally:
         print(question_builder.finalize())
@@ -125,25 +132,25 @@ def interval_training(count: int, tune: str, include_alterations: bool) -> None:
 
 @click.command()
 @click.option(
-    "--tune",
-    prompt="Tune from circle of fifth to practice",
+    '--tune',
+    prompt='Tune from circle of fifth to practice',
     default=lambda: random.choice(list(CIRCLE_OF_FIFTH_MAJOR_SCALES)),
 )
-@click.option("--count", prompt="How many questions", default=20)
+@click.option('--count', prompt='How many questions', default=20)
 @click.option(
-    "--include-alterations",
-    prompt="Include interval outside major scale",
+    '--include-alterations',
+    prompt='Include interval outside major scale',
     is_flag=True,
     default=True,
 )
 def main(tune: str, count: int, include_alterations: bool):
     if tune not in CIRCLE_OF_FIFTH_MAJOR_SCALES:
         raise ValueError(
-            f"Provide only tunes from circle of fifth: {CIRCLE_OF_FIFTH_MAJOR_SCALES.keys()}"
+            f"Provide only tunes from circle of fifth: {CIRCLE_OF_FIFTH_MAJOR_SCALES.keys()}",
         )
 
     interval_training(count, tune, include_alterations)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,24 +1,26 @@
+from __future__ import annotations
+
 import pytest
 
 from interval_training.main import get_interval
 
 
 @pytest.mark.parametrize(
-    "tune,interval,alteration,expected",
+    'tune,interval,alteration,expected',
     [
-        ("C", 2, "", "D"),
-        ("C", 2, "", "D"),
-        ("C", 2, "#", "D#"),
-        ("C", 2, "b", "Db"),
-        ("C", 2, "bb", "Dbb"),
-        ("C", 2, "x", "Dx"),
-        ("Db", 5, "b", "Abb"),
-        ("D", 3, "#", "Fx"),
-        ("D", 3, "b", "F"),
-        ("C", 9, "", "D"),
-        ("C", 11, "", "F"),
-        ("C", 13, "", "A"),
-        ("A", 6, "x", "Fx#"),
+        ('C', 2, '', 'D'),
+        ('C', 2, '', 'D'),
+        ('C', 2, '#', 'D#'),
+        ('C', 2, 'b', 'Db'),
+        ('C', 2, 'bb', 'Dbb'),
+        ('C', 2, 'x', 'Dx'),
+        ('Db', 5, 'b', 'Abb'),
+        ('D', 3, '#', 'Fx'),
+        ('D', 3, 'b', 'F'),
+        ('C', 9, '', 'D'),
+        ('C', 11, '', 'F'),
+        ('C', 13, '', 'A'),
+        ('A', 6, 'x', 'Fx#'),
     ],
 )
 def test_get_interval(tune, interval, alteration, expected):
@@ -26,16 +28,19 @@ def test_get_interval(tune, interval, alteration, expected):
 
 
 @pytest.mark.parametrize(
-    "tune,interval,alteration,expected",
+    'tune,interval,alteration,expected',
     [
-        ("C", 22, "", "Accepted intervals: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13]"),
         (
-            "G#",
+            'C', 22, '',
+            'Accepted intervals: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13]',
+        ),
+        (
+            'G#',
             2,
-            "",
-            "Provide only tunes from circle of fifth: dict_keys(["
+            '',
+            'Provide only tunes from circle of fifth: dict_keys(['
             "'C', 'G', 'F', 'D', 'Bb', 'A', 'Eb', 'E', 'Ab', 'B', 'Db', 'F#', 'Gb', 'Cb', 'C#'"
-            "])",
+            '])',
         ),
     ],
 )
